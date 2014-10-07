16a0be2728ef718933c4958d1f04b05847797014
rainysia <rainysia@gmail.com>
2014-10-07 17:29:30 +0800

    python note

---
+++ doc/python_study_code/input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+x = input("x: ")
+y = input("y: ")
+print "x*y result is",
+print(x*y)
+raw_input("Press <enter> to exit")
---
+++ doc/python_study_code/pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+x = 2**10
+print x
+print '--'*20
+print pow(2,10)
+
+
