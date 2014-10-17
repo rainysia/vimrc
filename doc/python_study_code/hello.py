c2d721cf199f618b398dd5222a2d3ad9e19aa7b0
rainysia <rainysia@gmail.com>
2014-10-17 11:23:04 +0800

    import

---
+++ doc/python_study_code/hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding=utf-8
+__metaclass__ = type
+print 'Hello, World!'
---
+++ doc/python_study_code/import.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+__metaclass__ = type
+import sys
+sys.path.append('/home/vc/git/vimrc/doc/python_study_code/')
+import hello
+
