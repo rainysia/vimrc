00f77310bbf26bdaec7aa51eda2e3ed62426c551
rainysia <rainysia@gmail.com>
2014-10-20 16:51:09 +0800

    Python time module

---
+++ doc/python_study_code/timep.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+import time
+print time.asctime()
+print time.mktime((2014,10,20,16,50,23,0,320,0))
+print time.localtime(1413795023.0)
+print time.strptime(time.asctime())
