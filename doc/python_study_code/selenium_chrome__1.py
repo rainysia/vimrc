a2ac9fd62f39e7770292e5be8bce98750fdb239d
rainysia <rainysia@gmail.com>
2014-10-27 18:05:04 +0800

    add selenium_ie remote

--- doc/python_study_code/selenium_chrome__1.py
+++ doc/python_study_code/selenium_chrome__1.py
@@ -21,25 +21,56 @@ browser.save_screenshot('screen.png')
 browser.quit()
 '''
 
+#remote chrome
+
+
+
 #remote IE
 import os
+# For Chinese
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-desired_cap = {'os': 'Windows', 'os_version': '2008', 'browser': 'IE', 'browser_version': '9.0', 'resolution' : '1024x768'}
+ie_desired_cap = {'os': 'Windows', 'os_version': '2008', 'browser': 'IE', 'browser_version': '9.0', 'resolution' : '1024x768'}
+tommy_remote_url = 'http://192.168.85.123:4444/wd/hub'
+derek_remote_url = 'http://192.168.87.72:18181/wd/hub'
+
 
 # command_executor = 'http://USERNAME:ACCESS_KEY@hub.xxx:80/wd/hub'
 driver = webdriver.Remote(
-    command_executor='http://192.168.87.72:4444/wd/hub',
-    desired_capabilities=desired_cap)
+    command_executor=derek_remote_url,
+    desired_capabilities=ie_desired_cap)
 
-driver.get("httpp://www.google.com")
-if not "Google" in driver.title:
-    raise Exception("Unable to load google page!")
-
-elem = driver.find_element_by_name("q")
-elem.send_keys("synnex")
-elem.submit()
+#google, name=q
+driver.get("http://www.baidu.com")
+eg_title = "百度"   #有中文,需要import sys   reload(sys)   sys.setdefaultencoding('utf-8')
 print driver.title
-driver.quit()
+#print help(driver)
+
+try:
+    if not eg_title in driver.title:
+        raise Exception("Unable to load ",eg_title," page!")
+
+    elem = driver.find_element_by_name("wd")
+    elem.send_keys("synnex")
+    elem.submit()
+    #two ways to wait, explict & implicit
+    #WebDriverWait.until(condition-that-finds-the-element)  #explict
+    #driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)    #implicit
+    print driver.title
+    sleep(10)
+    print '12345\n'
+except Exception, e:
+    raise e
+finally:
+    #driver.implicitly_wait(10)
+    #driver.set_script_timeout(10)
+    driver.quit()
+
+
